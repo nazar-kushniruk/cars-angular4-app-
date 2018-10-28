572779e4c3d4ec9ae47fea497f321d52f412de28
nazar-kushniruk <kushniruknazar@gmail.com>
2018-10-29 00:18:55 +0200

    add nested components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import {CarComponent} from './car/car.component';
+import { AddCarComponent } from './add-car/add-car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarComponent,
+    AddCarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/car/car.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit ,Input,Output} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+    selector: 'app-car',
+    templateUrl: './car.component.html',
+    styleUrls: ['./car.component.scss']
+})
+export class CarComponent implements OnInit {
+    carName = 'Ford';
+    carYear = 2017;
+@Input('carsItem') car :[{name: string, year: number}];
+@Output('putCar') car :[{name: string, year: number}];
+   /* @Input car: [{name: string, year: number}];*/
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+    
+}
