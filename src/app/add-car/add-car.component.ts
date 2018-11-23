55bbdafc09378fe76c5e508e9ea8e204ea749da4
nazar-kushniruk <kushniruknazar@gmail.com>
2018-11-23 21:18:24 +0200

    add flatpickr

--- src/app/add-car/add-car.component.ts
+++ src/app/add-car/add-car.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit,Output, EventEmitter} from '@angular/core';
-
+import flatpickr from "flatpickr";
 @Component({
     selector: 'add-car',
     templateUrl: './add-car.component.html',
@@ -10,6 +10,7 @@ export class AddCarComponent implements OnInit {
     carName = '';
     carYear = 2017;
   @Output('putCar')  carEmitter = new EventEmitter<{name: string, year: number}>();
+ // flatpickr(".year");
     constructor() {
     }
 
